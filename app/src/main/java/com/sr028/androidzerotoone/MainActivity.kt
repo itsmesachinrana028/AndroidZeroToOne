124911cf471660d1cdae3831c772968711b17851
Sachin Rana <sachin.rana@navixel.com>
2022-01-27 18:51:45 +0530

    initial set up

---
+++ app/src/main/java/com/sr028/androidzerotoone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sr028.androidzerotoone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
