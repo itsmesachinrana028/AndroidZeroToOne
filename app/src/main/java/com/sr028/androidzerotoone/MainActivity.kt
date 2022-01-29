b070abef76372cb116bf0e63e71e760e327c94d9
Sachin Rana <sachin.rana@navixel.com>
2022-01-29 10:13:39 +0530

    view binding

--- app/src/main/java/com/sr028/androidzerotoone/MainActivity.kt
+++ app/src/main/java/com/sr028/androidzerotoone/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sr028.androidzerotoone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sr028.androidzerotoone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val greeting = "Welcome to View Binding Tutorial"
+        val details = "In View Binding we can access view directly with out using findViewById()"
+        binding.tvGreetings.text = greeting
+        binding.tvDetails.text = details
     }
 }
